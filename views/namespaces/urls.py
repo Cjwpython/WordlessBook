4bb3780f97354799d52b9edbfc26cd0daa9f12b4
jiawei.chen <jiawei.chen@tophant.com>
2021-02-09 18:18:00 +0800

    添加命令空间的分页查询

--- views/namespaces/urls.py
+++ views/namespaces/urls.py
@@ -3,6 +3,6 @@ from apps import app
 from .views import *
 
 url_prefix = "/api/namespaces"
-app.add_url_rule(f"{url_prefix}", view_func=get_all_namespaces, methods=['GET', ], strict_slashes=False)
+app.add_url_rule(f"{url_prefix}", view_func=get_list_namespaces, methods=['GET', ], strict_slashes=False)
 app.add_url_rule("/api/namespace/<namespace_id>", view_func=get_single_namespace, methods=['GET', ], strict_slashes=False)
 app.add_url_rule("/api/namespace", view_func=Namespace.as_view("namespace"), strict_slashes=False)
--- views/namespaces/views.py
+++ views/namespaces/views.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import datetime
 import uuid
 from pprint import pprint
 
@@ -10,11 +11,26 @@ import logging
 logging.getLogger("test.views")
 
 
-def get_all_namespaces():
-    data = []
-    namespaces = namespaces_db.namespaces.find({}, {"name": 1, "nick_name": 1})  # 只返回名称和昵称
+def get_list_namespaces():
+    data = {}
+    data["namespaces"] = []
+    max_count = namespaces_db.namespaces.find().count()
+
+    params = request.args.to_dict(flat=True)
+    current_page = params.get("current_page", 1)
+    current_max_row = params.get("current_max_row", 15)
+    sort_type = params.get("sort_type", "create_time")
+    skip = (current_page - 1) * current_max_row
+    if sort_type not in ["create_time", "update_time"]:
+        sort_type = "create_time"
+    namespaces = namespaces_db.namespaces.find({}, {"name": 1, "nick_name": 1}).sort(sort_type, -1).skip(skip).limit(current_max_row)  # 只返回名称和昵称
     for namespace in namespaces:
-        data.append(namespace)
+        data["namespaces"].append(namespace)
+        print(namespace)
+    data["max_count"] = max_count
+    data["current_page"] = current_page
+    data["current_max_row"] = current_max_row
+    data["sort_type"] = sort_type
     return jsonify({"data": data}), 200
 
 
@@ -29,22 +45,26 @@ def get_single_namespace(namespace_id):
 class Namespace(views.MethodView):
 
     def post(self):
+        now_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         namespace = {
             "_id": str(uuid.uuid4()),
             "name": request.json.get("name"),
-            "nick_name": request.json.get("nick_name")
+            "nick_name": request.json.get("nick_name"),
+            "create_time": now_time,
+            "update_time": now_time
         }
         namespaces_db.namespaces.insert_one(namespace)
         return jsonify({"code": 201, "message": "创建成功"}), 201
 
     def put(self):
+        now_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         namespace_id = request.json.get("namespace_id")
         name = request.json.get("name")
         nick_name = request.json.get("nick_name")
         namespace = namespaces_db.namespaces.find_one({"_id": namespace_id})
         if not namespace:
             return jsonify({"code": 400, "message": "命名空间不存在"}), 400
-        namespaces_db.namespaces.update({"_id": namespace_id}, {"name": name, "nick_name": nick_name})
+        namespaces_db.namespaces.update({"_id": namespace_id}, {"$set": {"name": name, "nick_name": nick_name, "update_time": now_time}}, upsert=True)
         return jsonify({"code": 200, "message": "更新成功"}), 200
 
     def delete(self):
