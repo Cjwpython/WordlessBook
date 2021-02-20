3426ccf3ab2f8848caef98bbc7635407774d32b2
jiawei.chen <jiawei.chen@tophant.com>
2021-02-20 16:47:18 +0800

    修复应用的变更bug

--- views/applications/views.py
+++ views/applications/views.py
@@ -105,7 +105,7 @@ def application_change_env():
     current_env_id = data["current_env_id"]
     check_env_exist_by_id(env_id=env_id, raise_exist=False)
     check_env_exist_by_id(env_id=current_env_id, raise_exist=False)
-    application = check_application_exist_by_id(application_id=env_id, raise_exist=False)
+    application = check_application_exist_by_id(application_id=application_id, raise_exist=False)
     check_env_exist_application_by_id(env_id=env_id, application_id=application_id,application_name=application["name"])
     env_delete_application(env_id=current_env_id, application_id=application_id)
     update_application_env_id(application_id=application_id, env_id=env_id)
